6ebe57e6f50f8ae854f1c1adc42c00853f906d7b
sunrise <sunrise_sunrise_sunrise@outlook>
2017-12-01 15:34:16 +0800

    add

--- math.go
+++ math.go
@@ -51,6 +51,7 @@ func GetRandFloatTest() (ret float64) {
 	// return float64(r.Intn(100)) / 1000
 }
 
+//
 func Round(f float64, n int) float64 {
 	pow10_n := math.Pow10(n)
 	return math.Trunc((f+0.5/pow10_n)*pow10_n) / pow10_n
